d9481d4697c30c13bf3300235e3b25daf79e50f9
田口峻平 <taguchi@lifeistech.co.jp>
2018-11-04 18:31:06 +0900

    Initial Commit

---
+++ Itsudaregame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Itsudaregame
+//
+//  Created by 田口峻平 on 2018/11/04.
+//  Copyright © 2018年 田口峻平. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
